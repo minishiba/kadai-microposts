40b33b06ad3d16efb78959d5153f28ce812a0689
shiori.hidaka <shiori.a.hidaka@rakuten.com>
2018-06-18 05:26:22 +0000

    create MicropostsContoroller

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -15,9 +15,9 @@
         </aside>
         <div class="col-xs-8">
             <ul class="nav nav-tabs nav-justified">
-                <li><a href="#">TimeLine</a></li>
-                <li><a href="#">Followings</a></li>
-                <li><a href="#">Followers</a></li>
+                <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">TimeLine <span class="badge">{{ $count_microposts }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
             </ul>
              @if (Auth::id() == $user->id)
                   {!! Form::open(['route' => 'microposts.store']) !!}
