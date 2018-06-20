0705c92e97e0f793495935e8571e45391de66ed1
shiori.hidaka <shiori.a.hidaka@rakuten.com>
2018-06-20 01:34:02 +0000

    userfavcontroller

--- app/Http/Controllers/UserFavoriteController.php
+++ app/Http/Controllers/UserFavoriteController.php
@@ -5,37 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\User;
-use App\Micropost; // add
+
 
 class UserFavoriteController extends Controller
 {
      public function favorite(Request $request, $id)
     {
         \Auth::user()->favorite($id);
-        $favorites = $user->favorites()->paginate(10);
-
-        $data = [
-            'user' => $user,
-            'users' => $favorites,
-        ];
-
-        $data += $this->counts($user);
-        return view('users.favorites', $data);
+         return redirect()->back();
     }
 
     public function unfavorite($id)
     {
         \Auth::user()->unfavorite($id);
-        $user = User::find($id);
-        $unfavorites = $user->unfavorites()->paginate(10);
-
-        $data = [
-            'user' => $user,
-            'users' => $unfavorites,
-        ];
-
-        $data += $this->counts($user);
-
-        return view('users.unfavorites', $data);
+        return redirect()->back();
     }
 }
